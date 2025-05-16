83e4e0f073f43454b92d9adf6495f9c8ee299c25
Marchena1002 <andresmarchenaruiz42@gmail.com>
2025-05-16 16:18:08 -0600

    operaciones

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 
 using namespace std;
+double N1 = 0;
+double N2 = 0;
+	
 
-int main() {
-	
+int main(){
+
+
+
 	cout<<"Hello Class"<<endl;
-	
+	cout<<endl;
+	cout<<"Ingrese el primer numero:";
+	cin>>N1;
+	cout<<"Ingrese el segundo numero:";
+	cin>>N2;
+	cout<<endl;
+	cout<<"El resultado de la suma es: "<<N1+N2<<endl;
+	cout<<"El resultado de la resta es: "<<N1-N2<<endl;
+	cout<<"El resultado de la multiplicacion es: "<<N1*N2<<endl;
+	cout<<"El resultado de la division es: "<<N1/N2<<endl;
 	return 0;
 }
 
